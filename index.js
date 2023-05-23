7beee49d5b4d696e9142a0ad97fb58f3ed59f27e
RogelioSan09 <rogeliosnchz@gmail.com>
2023-05-23 03:09:27 -0700

    Set up user routes for getAllUsers and post for creating a new user. Set up route for getting a user by their 'id', route for put and delete. Set up route for deleting a user's friend or adding a new friend.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+// Set up for use of Express.js, requires the installation of the express package 
+// Sets up the connection to the database and starts the server.
+// Requires the created routes folder.
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Get the current working directory
 const cwd = process.cwd();
 
+// Set up port and initialize the app
 const PORT = process.env.port || 3001;
 const app = express();
 
+// Get the activity name from the current working directory
 const activity = cwd.includes('01-Activities')
   ? cwd.split('/01-Activities/')[1]
   : cwd;
 
+// Set up the middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// Connect to the database and run the server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
